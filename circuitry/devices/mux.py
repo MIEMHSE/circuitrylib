7febbaef4c6881fe9884477e07c93c85f6b07900
bug2bug <S.Sobko@profitware.ru>
2014-04-28 00:44:03 +0400

    Demultiplexer implementation

--- circuitry/devices/mux.py
+++ circuitry/devices/mux.py
@@ -48,5 +48,28 @@ class DeviceMux(Device):
 
 
 class DeviceDemux(Device):
-    mandatory_signals = ('strobe_signals', 'address_signals', 'input_signals', 'output_signals',)
+    mandatory_signals = ('strobe_signals', 'address_signals', 'data_signals', 'output_signals',)
     mandatory_signals_using_subs = ('strobe_signals',)
+
+    def __init__(self, **kwargs):
+        super(DeviceDemux, self).__init__(**kwargs)
+        address_and_data_minterms = list()
+        address_and_data_exludes = list()
+        self.truth_table = list()
+        self.functions = list()
+        for i in range(0, 2 ** len(self.address_signals)):
+            address_line = list()
+            for address_line_value in generate_binary_lines_current(len(self.address_signals), i):
+                address_line.append(address_line_value)
+            data_line = len(self.output_signals) * [0]
+            if i < len(data_line):
+                data_line[i] = 1
+                y_line = self.data_signals_truth_table
+                address_and_data_minterms.append(address_line + data_line)
+            else:
+                address_and_data_exludes.append(address_line + data_line)
+                y_line = map(lambda _y: 1 - _y, self.data_signals_truth_table)
+            self.truth_table.append((self.strobe_signals_truth_table, address_line, y_line, data_line))
+            if y_line[0] != 0:
+                address_and_data_function = SOPform(self.address_signals + self.data_signals, [address_line + y_line])
+                self.functions.append(self.strobe_signals_function & address_and_data_function)
