472fb2edc77b9635c66fe271f5176ba338c1a7cf
bug2bug <S.Sobko@profitware.ru>
2014-05-04 13:01:41 +0400

    Increment device

--- circuitry/devices/adder.py
+++ circuitry/devices/adder.py
@@ -11,13 +11,13 @@ from circuitry import generate_binary_lines_current
 from circuitry.devices import Device
 
 
-class DeviceAdder(Device):
+class DeviceAdd(Device):
     """Adder device"""
     mandatory_signals = ('strobe_signals', 'first_signals', 'second_signals', 'output_signals',)
     truth_table_signals = ('strobe_signals', 'first_signals', 'second_signals', 'output_signals',)
 
     def __init__(self, **kwargs):
-        super(DeviceAdder, self).__init__(**kwargs)
+        super(DeviceAdd, self).__init__(**kwargs)
         function_s = lambda vx, vy, vp: Xor(vx, vy, vp)
         function_p = lambda vx, vy, vp: Or(And(vx, vy), And(vx, vp), And(vy, vp))
         self.functions = list()
@@ -37,10 +37,10 @@ class DeviceAdder(Device):
         self.functions.append(self.strobe_signals_function & current_p)
         self.truth_table = list()
         input_signals_len = len(self.first_signals) + len(self.second_signals)
-        for i in range(0, 2 ** input_signals_len):
+        for bin_i in range(0, 2 ** input_signals_len):
             first_line, second_line = list(), list()
             value_index = 0
-            for line_value in generate_binary_lines_current(input_signals_len, i):
+            for line_value in generate_binary_lines_current(input_signals_len, bin_i):
                 if value_index < len(self.first_signals):
                     first_line.append(line_value)
                 else:
@@ -56,3 +56,27 @@ class DeviceAdder(Device):
             for i in range(0, len(self.output_signals)):
                 y_line.append(1 if self.functions[i].subs(line_subs) else 0)
             self.truth_table.append((self.strobe_signals_truth_table, first_line, second_line, y_line))
+
+
+class DeviceInc(Device):
+    """Increment device"""
+    mandatory_signals = ('strobe_signals', 'data_signals', 'output_signals',)
+    truth_table_signals = ('strobe_signals', 'data_signals', 'output_signals',)
+
+    def __init__(self, **kwargs):
+        super(DeviceInc, self).__init__(**kwargs)
+        inc_dict = kwargs.copy()
+        inc_dict.update({
+            'first_signals': 't:1',
+            'second_signals': kwargs['data_signals']
+        })
+        inc_adder = DeviceAdd(**inc_dict)
+        self.functions = list()
+        for function in inc_adder.functions[:len(self.output_signals)]:
+            self.functions.append(function.subs({'t0': 1}))
+        self.truth_table = list()
+        for truth_table_line in inc_adder.truth_table:
+            _, first_line, second_line, y_line = truth_table_line
+            if first_line[0]:
+                self.truth_table.append((self.strobe_signals_truth_table, second_line,
+                                         y_line[:len(self.output_signals)]))
